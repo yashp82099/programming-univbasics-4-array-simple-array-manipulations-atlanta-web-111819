51698d30183c2dca47ea21cc692ee021eb9dcd90
yash patel <yash82099@yahoo.com>
2019-10-10 22:33:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,20 @@ def using_push(array, string)
   array.push(string)
 end
 
+def using_unshift(array.string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+  
+  
+def shift_with_args(array)
+  array.shift()
+  array.shift()
+end
