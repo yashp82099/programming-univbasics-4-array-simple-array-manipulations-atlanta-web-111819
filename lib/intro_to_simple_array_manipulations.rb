794d2ab9627e85095eec37863dce2d4a7c88e82a
yash patel <yash82099@yahoo.com>
2019-10-10 22:49:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,6 +34,27 @@ def using_uniq(array)
   array.uniq()
 end
 
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete(int)
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
